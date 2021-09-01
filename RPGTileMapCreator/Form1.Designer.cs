7fe9ad72a0557981c2db38e0e59d0f0da12f8657
Randy Rieger <60799555+randyjrieger@users.noreply.github.com>
2021-08-31 20:29:37 -0400

    Initial commit

---
+++ RPGTileMapCreator/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace RPGTileMapCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Panel_Palete = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnWriteFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Panel_Palete
+            // 
+            this.Panel_Palete.AutoScroll = true;
+            this.Panel_Palete.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Panel_Palete.Location = new System.Drawing.Point(660, 41);
+            this.Panel_Palete.Name = "Panel_Palete";
+            this.Panel_Palete.Size = new System.Drawing.Size(501, 464);
+            this.Panel_Palete.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(683, 13);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(789, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnWriteFile
+            // 
+            this.btnWriteFile.Location = new System.Drawing.Point(991, 13);
+            this.btnWriteFile.Name = "btnWriteFile";
+            this.btnWriteFile.Size = new System.Drawing.Size(75, 23);
+            this.btnWriteFile.TabIndex = 3;
+            this.btnWriteFile.Text = "Wrtie";
+            this.btnWriteFile.UseVisualStyleBackColor = true;
+            this.btnWriteFile.Click += new System.EventHandler(this.btnWriteFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 504);
+            this.Controls.Add(this.btnWriteFile);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Panel_Palete);
+            this.Name = "Form1";
+            this.Text = "RPG Tile Map Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Panel_Palete;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnWriteFile;
+    }
+}
+
---
+++ RPGTileMapCreator/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPGTileMapCreator
+{
+    public partial class Form1 : Form
+    {
+        PictureBox SelectedPaletteBox = new PictureBox();
+        PictureBox formerSelected = new PictureBox();
+
+        public Form1()
+        {
+            PictureBox p;
+            InitializeComponent();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    p = new PictureBox();
+
+                    p.BackColor = Color.Ivory;
+                    p.SizeMode = PictureBoxSizeMode.StretchImage;
+                    p.Width = 51;
+                    p.Height = 51;
+                    p.Left = 51 * i - 51;
+                    p.Top = 51 * j - 51;
+                    p.Name = p.Left.ToString() + "_" + p.Top.ToString();
+                    p.BorderStyle = BorderStyle.FixedSingle;
+                    p.MouseDown += (s, args) =>
+                    {
+                        if (args.Button == MouseButtons.Right)
+                        {
+                            CanvasBox_RightClick(s, args);
+                        }
+                        else if (args.Button == MouseButtons.Left)
+                        {
+                            CanvasBox_Click(s, args);
+                        }
+                    };
+                    //  p.DragOver += new EventHandler(CanvasBox_DragOver);
+                    this.Controls.Add(p);
+                }
+            }
+
+            int row = 0;
+            int col = 0;
+            foreach (string s in Directory.GetFiles(@"C:\git\thievesGuild\ThievesGuild\ThievesGuild\Resources\tiles"))
+            {
+                p = new PictureBox();
+
+                p.BackColor = Color.Ivory;
+                p.SizeMode = PictureBoxSizeMode.StretchImage;
+                p.Width = 51;
+                p.Height = 51;
+
+                p.Left = col * 51;
+                p.Top = row * 51;
+                col++;
+                if (col == 8)
+                {
+                    col = 0; row++;
+                }
+
+
+                p.Name = Path.GetFileName(s);
+                p.BorderStyle = BorderStyle.None;
+                p.Image = Image.FromFile(s);
+                p.Click += new EventHandler(PaletteBox_Click);
+                Panel_Palete.Controls.Add(p);
+            }
+
+        }
+        public void CanvasBox_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            p.Image = SelectedPaletteBox.Image;
+            p.Tag = SelectedPaletteBox.Tag;
+        }
+
+        public void CanvasBox_RightClick(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            p.Tag = null;
+            p.Image = null;
+        }
+
+        public void PaletteBox_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            formerSelected.BorderStyle = BorderStyle.None;
+
+            p.BorderStyle = BorderStyle.Fixed3D;
+            textBox1.Text = (string) p.Tag;
+            textBox1.Focus();
+            SelectedPaletteBox = p;
+            formerSelected = p;
+        }
+        public void PaletteBox_LostFocus(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            p.BorderStyle = BorderStyle.FixedSingle;
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SelectedPaletteBox.Tag = (string) textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void btnWriteFile_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            foreach (PictureBox p in this.Controls)
+            {
+                if (p.Tag != null)
+                    s = s + ((string)p.Tag);
+
+                MessageBox.Show(s);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          //  SelectedPaletteBox.Tag = (string)textBox1.Text;
+        }
+    }
+}
